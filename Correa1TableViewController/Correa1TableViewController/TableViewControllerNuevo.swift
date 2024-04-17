5f7479171c351b050aae211dd54966d366847523
oliver Correa Cabana <olivercorreacabana@outlook.es>
2024-04-17 13:35:48 -0500

    Lab6 part 1 till number 55, real

--- Correa1TableViewController/Correa1TableViewController/TableViewControllerNuevo.swift
+++ Correa1TableViewController/Correa1TableViewController/TableViewControllerNuevo.swift
@@ -19,6 +19,7 @@ class TableViewControllerNuevo: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.isEditing = true
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
