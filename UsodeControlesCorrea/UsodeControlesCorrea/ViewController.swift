406da9b908f2beb77b1afdc8825504dffb984d86
oliver Correa Cabana <olivercorreacabana@outlook.es>
2024-04-23 22:59:42 -0500

    Lab7 hasta el punto 4 completado

---
+++ UsodeControlesCorrea/UsodeControlesCorrea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsodeControlesCorrea
+//
+//  Created by oliver Correa Cabana on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
