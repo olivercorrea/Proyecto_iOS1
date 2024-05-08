afe3957dfec89c360bd4bfcd669ef0883465456e
oliver1 <oliver1@MacBook-Pro-de-Mac-2.local>
2024-05-08 11:40:52 -0500

    CorreaColeccionDeJuego first commit

---
+++ CorrreaColeccionDeJuegos/CorrreaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CorrreaColeccionDeJuegos
+//
+//  Created by oliver1 on 8/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
